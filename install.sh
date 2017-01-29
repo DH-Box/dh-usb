3cf7da3624554031f7dbb80ee9066ac92349b0a0
Jonathan Reeve <jon.reeve@gmail.com>
2017-01-29 17:46:54 -0500

    update for changes in Arch

--- install.sh
+++ install.sh
@@ -30,13 +30,13 @@ dir=$PWD
 
 sys="crda dialog gpm grml-zsh-config linux-atm lsscsi mc mtools ndisc6 nfs-utils grub efibootmgr nilfs-utils ntp rp-pppoe sg3_utils smartmontools speedtouch wvdial xl2tpd hdparm gptfdisk exfat-utils dmraid dosfstools hfsprogs gnu-netcat sdparm refind-efi f2fs-tools ntfs-3g testdisk partclone fsarchiver btrfs-progs parted ddrescue partimage wireless_tools wpa_actiond vpnc ppp pptpclient openconnect openssh openvpn dnsmasq dnsutils tcpdump lftp darkhttpd dhclient ethtool b43-fwcutter ipw2100-fw ipw2200-fw zd1211-firmware rsync rfkill usb_modeswitch mesa mesa-vdpau xf86-input-synaptics xf86-video-ati xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-openchrome xf86-video-vesa xf86-video-vmware xorg-server xorg-server-utils xorg-xinit xorg-xauth alsa-utils expect git pkgfile powerpill reflector sudo unzip unrar wget xdg-user-dirs-gtk yaourt fakeroot broadcom-wl" 
 
-desktop="gdm baobab chromium chromium-pepper-flash eog espeak evince file-roller gedit gnome-backgrounds gnome-bluetooth gnome-calculator gnome-common gnome-contacts gnome-control-center gnome-disk-utility gnome-documents gnome-keyring gnome-logs gnome-maps gnome-photos gnome-screenshot gnome-shell gnome-shell-extension-dash-to-dock gnome-shell-extensions gnome-sound-recorder gnome-system-monitor gnome-terminal gnome-themes-standard gnome-tweak-tool gnome-weather gnome-boxes gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer0.10-plugins gstreamer-vaapi gvfs-mtp libgnomeui libwnck3 nautilus network-manager-applet totem transmission-cli transmission-gtk arc-gtk-theme gnome-software" 
+desktop="gdm baobab chromium chromium-pepper-flash eog espeak evince file-roller gedit gnome-backgrounds gnome-bluetooth gnome-calculator gnome-common gnome-contacts gnome-control-center gnome-disk-utility gnome-documents gnome-keyring gnome-logs gnome-maps gnome-photos gnome-screenshot gnome-shell gnome-shell-extension-dash-to-dock gnome-shell-extensions gnome-sound-recorder gnome-system-monitor gnome-terminal gnome-themes-standard gnome-tweak-tool gnome-weather gnome-boxes gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer-vaapi gvfs-mtp libwnck3 nautilus networkmanager network-manager-applet totem transmission-cli transmission-gtk arc-gtk-theme gnome-software" 
 
 fonts="divehi-fonts ttf-aboriginal-sans ttf-arphic-uming ttf-baekmuk ttf-bitstream-vera ttf-dejavu ttf-freefont ttf-opensans" 
 
 dh="vim python python-pip ruby jupyter jupyter-notebook python-nltk pandoc pandoc-citeproc pandoc-crossref mathjax" 
 
-hacker="gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer0.10-plugins gstreamer-vaapi i3-wm i3status qutebrowser zathura" 
+hacker="gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer-vaapi i3-wm i3status qutebrowser zathura" 
 
 ruby_gems="jekyll"
 
@@ -112,7 +112,7 @@ function install_hacker {
 
 function config_init { 
 	echo "Starting initial configuration of system." 
-	#arch-chroot /mnt ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+	arch-chroot /mnt ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 
 	# Copy /etc files
 	for file in locale.gen locale.conf hostname fstab makepkg.conf
@@ -202,7 +202,7 @@ function install_big {
 	# we have to supply a special CFLAGS argument. See 
 	# https://github.com/GregBowyer/cld2-cffi/issues/12
 	# for the status on this bug. 
-	arch-chroot /mnt CFLAGS="-Wno-narrowing" pip install -U cld2-cffi
+	arch-chroot /mnt env CFLAGS="-Wno-narrowing" pip install -U cld2-cffi
 	arch-chroot /mnt pip install textacy
 } 
 
